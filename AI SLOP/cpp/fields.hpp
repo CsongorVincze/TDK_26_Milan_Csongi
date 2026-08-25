a8e6ab458d54a57e60f770fb0071e65ac22f9a11
CsongorVincze <vinczefarkascsongor@gmail.com>
2026-08-25 12:57:34 +0200

    csongi sim nincs sponge

--- AI SLOP/cpp/fields.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-// The dynamical state of the system on the radial grid.
-struct State {
-    std::vector<double> phi;   // field
-    std::vector<double> pi;    // d(phi)/dt (conjugate momentum)
-};
--- AI SLOP/cpp/initial_conditions.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "fields.hpp"
-
-// Sets phi(r,0) = amp0 * exp(-r^2/width0^2), pi(r,0) = 0 (see params.hpp).
-void set_initial_conditions(State& s);
